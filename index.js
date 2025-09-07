a752be0c732c345f2a720e6a07a6fb6c972d470a
suresh4478v <sureshdas99760@gmail.com>
2025-09-07 17:59:11 +0530

    fix: use process.env.PORT with fallback 4000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000 //modify port if needed
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
